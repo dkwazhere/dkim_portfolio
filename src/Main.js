3a32f24900f374d0e35456efa80fee6c66663d6a
dkwazhere <dhealth77@gmail.com>
2018-07-25 17:13:04 -0700

    added components(aboutme, contact, home, portfolio, resume)

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
+import Home from './Components/Home';
+import AboutMe from './Components/AboutMe';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import { Route, NavLink, HashRouter} from 'react-router-dom';
+
+
 
 class Main extends Component {
   render() {
     return(
-      <div>
-        <h1>Simple SPA</h1>
-        <ul className="header">
-          <li><a href="/">Home</a></li>
-          <li><a href="/stuff">About Me</a></li>
-          <li><a href="/contact">Resume</a></li>
-          <li><a href="/stuff">Portfolio</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      <div className="content">
+      <HashRouter>
+        <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/aboutme">About Me</NavLink></li>
+            <li><NavLink to="/resume">Resume</NavLink></li>
+            <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
 
-  </div>
-</div>
+          <div className="content">
+            <Route path="/" component={Home}/>
+            <Route path="/aboutme" component={AboutMe}/>
+            <Route path="/resume" component={Resume}/>
+            <Route path="/portfolio" component={Portfolio}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
     )
   }
 }
