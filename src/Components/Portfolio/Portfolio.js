d80775135b19d0d90a8feba7959287d40d516717
dkwazhere <dhealth77@gmail.com>
2018-07-31 15:32:20 -0700

    added portfolio component, added three cards :

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import './Portfolio.css';
-import { Row, Container } from "reactstrap";
+import { Col, Row, Container, Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 import glasseslogo from '../../assets/images/glasses.png';
+import ikdesignlogo from '../../assets/images/ikdesign.png';
+import jepbyjeplogo from '../../assets/images/jepbyjep.png';
+import staytunedlogo from '../../assets/images/staytuned.jpg';
 
 class Portfolio extends Component {
   render() {
@@ -12,6 +16,46 @@ class Portfolio extends Component {
         </Row>
         <Row>
           <h2 id="portfolio-header">portfolio</h2>
+        </Row>
+        <Row>
+          <Col>
+            <div className="project-cards">
+              <Card className="card1">
+                <CardImg top width="15%" src={staytunedlogo} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>Coming soon...</CardTitle>
+                  <CardSubtitle></CardSubtitle>
+                  <CardText></CardText>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
+          <Col>
+            <div className="project-cards">
+              <Card className="card2">
+                <CardImg top width="15%" src={ikdesignlogo} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>IK Design</CardTitle>
+                  <CardSubtitle>Front End Development</CardSubtitle>
+                  <CardText>A portfolio built for a family friend. It was built using React.js.</CardText>
+                  <Button>Github</Button> <Button>Live</Button>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
+          <Col>
+            <div className="project-cards">
+              <Card className="card3">
+                <CardImg top width="15%" src={jepbyjeplogo} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle>JepByJep</CardTitle>
+                  <CardSubtitle>Front End Development</CardSubtitle>
+                  <CardText>A simplistic and stunning Ecommerce Site built using React.js and shopify API.</CardText>
+                  <Button>Github</Button> <Button>Live</Button>
+                </CardBody>
+              </Card>
+            </div>
+          </Col>
         </Row>
       </Container>
     );
